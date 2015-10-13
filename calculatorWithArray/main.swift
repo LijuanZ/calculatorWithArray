f2b6f96672a11658d0d38be15a2c910dc38ddd31
Lijuan Zhang <zhanglj@uw.edu>
2015-10-12 21:22:41 -0700

    Initial Commit

---
+++ calculatorWithArray/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  calculatorWithArray
+//
+//  Created by Keran Zheng on 10/12/15.
+//  Copyright © 2015 Lijuan Zhang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
